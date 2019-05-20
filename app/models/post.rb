397296bffc6445a276029ef951a003839cceba74
Daniel Schneider <daschne8@gmail.com>
2019-05-20 14:58:39 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,5 @@ class Post < ActiveRecord::Base
   validates :title, presence: true
   validates :content, length: {minimum: 100}
   validates :summary, length: {maximum: 15}
+  validates :category, presence: true, if: ["Fiction"]
 end
