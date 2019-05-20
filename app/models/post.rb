60f9e0ec28e60dd5f64a92fba1e03a52a6d0b0d0
Daniel Schneider <daschne8@gmail.com>
2019-05-20 15:04:25 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,8 +3,9 @@ class Post < ActiveRecord::Base
   validates :content, length: {minimum: 100}
   validates :summary, length: {maximum: 15}
   validates :validate_category
+  
   def validate_category
-    if !["Fiction"].include(:category)
+    if !(["Fiction"]).include?(:category)
       errors.add(:category, :invalid)
     end
   end
