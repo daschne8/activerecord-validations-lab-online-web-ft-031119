7f6349b7625b30a25488d794f2b86f82ceaf74ab
Daniel Schneider <daschne8@gmail.com>
2019-05-20 15:14:43 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,7 @@ class Post < ActiveRecord::Base
   validates :content, length: {minimum: 100}
   validates :summary, length: {maximum: 15}
   validates_inclusion_of :category, in: ["Fiction"]
+  validates :title, format: {with: /(Won't Believe|Secret|Top \d+|Guess)/, message: "use clickbait title"}
 
 
 
